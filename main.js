39f8fbd24f03f94b3768197f75bb3f6c642f275b
vogelsara <sari.vogel.94@gmail.com>
2019-02-17 13:57:06 +0100

    Implement loadPage()

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
 function loadPage() {
-    
+    initializeGame();
+    showEntireBoard();
+}
+
+function initializeGame() {
+    fetch("/createCardList.php.php", {method: 'POST'});
+}
+
+function showEntireBoard() {
+
 }
