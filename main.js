20f4a4a8d7fc6e4961ede76824af9450ebe04cb6
vogelsara <sari.vogel.94@gmail.com>
2019-02-20 00:45:54 +0100

    Clean main.js

--- main.js
+++ main.js
@@ -13,24 +13,20 @@ function showEntireBoard() {
         method: 'POST'
     }).then((response) => response.json())
     .then((numberOfCards) => {
-        if (numberOfCards) {
 
-            var container = document.getElementsByClassName("cardContainer")[0]
+        var container = document.getElementsByClassName("cardContainer")[0]
 
-            for(var i = 1; i <= numberOfCards; i++) {
-                
-                var cardDiv = document.createElement("div")
-                cardDiv.id = "n" + i;
-                cardDiv.setAttribute('onclick', 'onCardClick(this)')
-                
-                cardDiv.style.backgroundColor = '#bccce5';
-        
-                cardDiv.className = "memoryCardBack"
-                container.appendChild(cardDiv)
-            }
+        for(var i = 1; i <= numberOfCards; i++) {
+            
+            var cardDiv = document.createElement("div")
+            cardDiv.id = "n" + i;
+            cardDiv.setAttribute('onclick', 'onCardClick(this)')
+            cardDiv.style.backgroundColor = '#bccce5';
+            cardDiv.className = "memoryCardBack"
+            container.appendChild(cardDiv)
+
         }
     });
-
 }
 
 function onCardClick(card) {
@@ -47,22 +43,19 @@ function onCardClick(card) {
         body: formData
     }).then((response) => response.json())
     .then((changedCards) => {
-        if (changedCards) {
-            var changedCardIds = Object.keys(changedCards)
-            for(var i = 0; i < changedCardIds.length; i++) {
-                var changedCard = document.getElementById("n" + changedCardIds[i])
-                if (changedCards[changedCardIds[i]] == "") {
 
-                    changedCard.style.backgroundColor = '#bccce5';
-                    changedCard.style.backgroundImage = "none";
+        var changedCardIds = Object.keys(changedCards)
 
-                } else {
+        for(var i = 0; i < changedCardIds.length; i++) {
 
-                    changedCard.style.transition = "all 2s ease"
-                    changedCard.style.backgroundImage = "url('./img/" + changedCards[changedCardIds[i]] + "')";
-                    changedCard.style.backgroundSize = "cover"
+            var changedCardNumber = changedCardIds[i];
+            var changedCardDiv = document.getElementById("n" + changedCardNumber);
+            var changedCardImage = changedCards[changedCardNumber];
 
-                }
+            if (changedCardImage == "") {
+                changedCardDiv.style.backgroundImage = "none";
+            } else {
+                changedCardDiv.style.backgroundImage = "url('./img/" + changedCardImage + "')";
             }
         }
     });
