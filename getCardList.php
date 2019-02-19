0254474c5cf1b3bcafdbafbb3978fb8e7ab4933f
vogelsara <sari.vogel.94@gmail.com>
2019-02-19 19:49:11 +0100

    Show card backsides on index

---
+++ getCardList.php
@@ -0,0 +1,17 @@
+<?php
+
+session_start();
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if (isset($_SESSION["cardList"])) {
+        $serializedCardList = $_SESSION["cardList"];
+        $cardList = unserialize($serializedCardList);
+        echo json_encode($cardList);
+    } else {
+        echo json_encode("");
+    }
+} else {
+    http_response_code(400);
+}
+
+?>
--- main.js
+++ main.js
@@ -9,4 +9,29 @@ function initializeGame() {
 
 function showEntireBoard() {
 
+    fetch("/getCardList.php", {
+        method: 'POST'
+    }).then((response) => response.json())
+    .then((cardListJson) => {
+        if (cardListJson) {
+            var container = document.getElementsByClassName("cardContainer")[0]
+
+            var n = cardListJson.length;
+
+            for(var i = 1; i <= n; i++) {
+                
+                var cardDiv = document.createElement("div")
+                cardDiv.setAttribute('index', i)
+                cardDiv.setAttribute('onclick', 'onCardClick(this)')
+        
+                cardDiv.className = "memoryCardBack"
+                container.appendChild(cardDiv)
+            }
+        }
+    });
+
+}
+
+function onCardClick(card) {
+    
 }
