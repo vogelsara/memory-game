b72740d1f254a1494a68fb7d8d1a7a06b92f1407
vogelsara <sari.vogel.94@gmail.com>
2019-02-19 21:43:31 +0100

    Remove isset check of SESSION in onCardClick

--- onCardClick.php
+++ onCardClick.php
@@ -5,16 +5,12 @@ session_start();
 function onClick($id) {
     $serializedCardList = $_SESSION["cardList"];
     $cardList = unserialize($serializedCardList);
-    
+
 }
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    if (isset($_SESSION["cardList"])) {
-        $id = $_POST["id"];
-        onClick($id);
-    } else {
-        echo json_encode("");
-    }
+    $id = $_POST["id"];
+    onClick($id);
 } else {
     http_response_code(400);
 }
